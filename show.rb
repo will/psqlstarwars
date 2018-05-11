cafec5e2a4fd405df3f46cc1a9f40e987921b007
Will Leinweber <will@bitfission.com>
2018-05-11 13:17:46 -0700

    progress

--- show.rb
+++ show.rb
@@ -87,20 +87,59 @@ $ du -hs site.html
 EOF
 }
 
-start = (ARGV[0] || 1).to_i - 1
+slides << ->() {
+  nl = ->(s) { s.gsub('\n', pastel.cyan('\n')) }
+  puts head "downloaded site"
+  puts pastel.dim.italic "… html and js"
+  puts nl.(%q(var film = ') + pastel.magenta(2) + '\n\n\n\n\n\n\n\n\n\n\n\n\n\n' + pastel.magenta(15) + '\n\n\n\n\n                       WWW.ASCIIMATION.CO.NZ\n')
+  puts pastel.dim.italic "  … and on and on"
+  puts nl.("  " + pastel.magenta(14) + %q{\n====\n Help me, o o~~\n Obi-Wan Kenobi!  _\\- /_\n ___ / \\ / \\\n /() \\ //| | |\\\\\n _|_____|_ // | | |//\n ,@ | | === | | // | | //\n /=- |_| O |_|(\' |===(|\n || || O || | || |\n || ||__*__|| (_)(_)\n --~~--- |~ \\___/ ~| |_||_|\n | | /=\\ /=\\ /=\\ |_||_|\n_________|_____|______[_]_[_]_[_]____/__][__\\______________________\n} +
+    pastel.magenta(15) + %q{\n====\n You\'re my only o o~~\n hope!  _\\- /_\n })
+  puts pastel.dim.italic "  … and on and on"
+  puts nl.('www.asciimation.co.nz\n\n\n\n\n' + pastel.magenta('1') + %q(\n\n\n\n\n\n\n\n\n\n\n\n\n\n�\n'.split('\n'); ))
+  puts pastel.dim.italic "… more html and js"
+}
 
-i = start
+
+slides << ->() {
+  puts head "postgres schema"
+  c = pastel.cyan.bold.detach
+  puts "CREATE TABLE film ("
+  puts "  #{c.('i')}     serial PRIMARY KEY,"
+  puts "  #{c.('count')} int    NOT NULL,"
+  puts "  #{c.('frame')} text   NOT NULL"
+  puts ");"
+}
+
+slides << ->() {
+  puts head "ETL!"
+
+}
+
+last_from_file = File.read(".current") rescue nil
+@current = (ARGV[0] || last_from_file || 1).to_i - 1
+
+Signal.trap("INT") do
+  File.open(".current", "w") { |f| f.write @current+1 }
+  puts "saved current slide"
+  exit
+end
+
+
 loop do
-  break if i >= slides.size
+  break if @current >= slides.size
   print "\e[H\e[2J"
-  puts pastel.dim.italic "slide #{i+1}/#{slides.size}"
+  puts pastel.dim.italic "slide #{@current+1}/#{slides.size}"
 
-  slides[i].call
+  slides[@current].call
 
   input = STDIN.gets.strip
   if input == "b"
-    i -= 1
+    @current -= 1
   else
-    i += 1
+    @current += 1
   end
 end
+
+File.delete ".current" if File.exists? ".current"
+puts 'show over \o/'
